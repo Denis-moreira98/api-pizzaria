f25ee00cfaf33fc1d0dd8d26fae5245271344d44
Denis Moreira <silvad281@gmail.com>
2024-04-30 18:29:36 -0300

    feat: logando usuário  e gerando token JWT

--- src/service/user/AuthUserService.ts
+++ src/service/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest {
    email: string;
@@ -23,6 +24,25 @@ class AuthUserService {
       if (!passwordMatch) {
          throw new Error("password incorrect");
       }
+
+      // Se deu tudo certo, gerar o token do usuário
+      const token = sign(
+         {
+            name: user.name,
+            email: user.email,
+         },
+         process.env.JWT_SECRET as string,
+         {
+            subject: user.id,
+            expiresIn: "15d",
+         }
+      );
+      return {
+         id: user.id,
+         name: user.name,
+         email: user.email,
+         token: token,
+      };
    }
 }
 
