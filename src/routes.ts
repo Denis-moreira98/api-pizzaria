aaebbb3a5e5242c0ea7c8239ffb2dcfb5221b28f
Denis Moreira <silvad281@gmail.com>
2024-05-01 21:01:48 -0300

    feat: add rota para cadastro de category

--- src/routes.ts
+++ src/routes.ts
@@ -7,11 +7,20 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 // Middleware
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+
 const router = Router();
 
 // -- ROTAS USER --
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+// -- ROTAS CATEGORY --
+router.post(
+   "/category",
+   isAuthenticated,
+   new CreateCategoryController().handle
+);
 
 export { router };
