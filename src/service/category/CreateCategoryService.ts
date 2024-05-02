33d21ed8d7a06bebc2802c557204f899bd174e1a
Denis Moreira <silvad281@gmail.com>
2024-05-01 21:03:28 -0300

    feat: criando service para cadastrar category no banco

---
+++ src/service/category/CreateCategoryService.ts
@@ -0,0 +1,36 @@
+import prismaClient from "../../prisma";
+
+interface CategoryRequest {
+   name: string;
+}
+
+class CreateCategoryService {
+   async execute({ name }: CategoryRequest) {
+      if (name === "") {
+         throw new Error("Name invalid");
+      }
+
+      const categoryAlreadyExists = await prismaClient.category.findFirst({
+         where: {
+            name: name,
+         },
+      });
+      if (categoryAlreadyExists) {
+         throw new Error("The category Already exists");
+      }
+
+      const category = await prismaClient.category.create({
+         data: {
+            name: name,
+         },
+         select: {
+            id: true,
+            name: true,
+         },
+      });
+
+      return category;
+   }
+}
+
+export { CreateCategoryService };
