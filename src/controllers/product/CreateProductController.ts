66bc75ad2a8f086c77d24b24dd0ef09e93f68537
Denis Moreira <silvad281@gmail.com>
2024-05-02 12:39:48 -0300

    feat: criando controller para cadastro de  product

---
+++ src/controllers/product/CreateProductController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { CreateProductService } from "../../service/product/CreateProductService";
+
+class CreateProductController {
+   async handle(req: Request, res: Response) {
+      const { name, price, description, category_id } = req.body;
+
+      const createProductService = new CreateProductService();
+
+      if (!req.file) {
+         throw new Error("error upload file required");
+      } else {
+         const { originalname, filename: banner } = req.file;
+
+         const product = await createProductService.execute({
+            name,
+            price,
+            description,
+            banner,
+            category_id,
+         });
+         return res.status(201).json(product);
+      }
+   }
+}
+export { CreateProductController };
