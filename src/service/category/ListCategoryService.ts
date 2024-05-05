61028c098b2a2c5e4296a6c42e194ebb0cc44870
Denis Moreira <silvad281@gmail.com>
2024-05-04 21:32:37 -0300

    feat: organizando as category por ordem desc

--- src/service/category/ListCategoryService.ts
+++ src/service/category/ListCategoryService.ts
@@ -7,6 +7,9 @@ class ListCategoryService {
             id: true,
             name: true,
          },
+         orderBy: {
+            created_at: "desc",
+         },
       });
 
       return category;
